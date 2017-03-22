58dc322d4078ac4d1b82061e317ee0d397b9dc94
谢桂纲 <xie.guigang@gcmodeller.org>
2017-03-22 16:28:28 +0800

    compile drugbank mysql ORM class

--- mysql/build.bat
+++ mysql/build.bat
@@ -1,6 +1,6 @@
 REM converts the mysql dataabase schema source code to visualbasic source code
-reflector --reflects -o "./drugbank/" /namespace "mysql" /split --language=visualbasic /sql ./drugbank.sql
-reflector --reflects -o "./cosmic/" /namespace "mysql" /split --language=visualbasic /sql ./cosmic.sql
+"D:\GCModeller\tools\mysql\Reflector.exe" --reflects -o "./drugbank/" /namespace "mysql" /split --language=visualbasic /sql ./drugbank.sql
+"D:\GCModeller\tools\mysql\Reflector.exe" --reflects -o "./cosmic/" /namespace "mysql" /split --language=visualbasic /sql ./cosmic.sql
 
 REM copy ORM source code to project source code directory
 RD /S /Q "../DrugBank/mysql/"
