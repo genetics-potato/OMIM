08e74bcf5df6944faef471842f53143d307bc395
xieguigang <xie.guigang@gmail.com>
2017-04-02 22:51:09 +0800

    dump cosmic tsv as mysql

---
+++ cosmic-dump/Program.vb
@@ -0,0 +1,19 @@
+﻿Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports SMRUCC.Clinic.Data
+
+Module Program
+
+    Public Function Main() As Integer
+        Return GetType(Program).RunCLI(App.CommandLine)
+    End Function
+
+    <ExportAPI("/Imports.Cosmic.Tsv",
+               Usage:="/Imports.Cosmic.Tsv /in <tsv.DIR> [/out <mysql.DIR>]")>
+    Public Function ImportsCosmic(args As CommandLine) As Integer
+        Dim in$ = args <= "/in"
+        Dim out As String = args.GetValue("/out", [in].TrimDIR & ".mysql_Dumps/")
+        Call Cosmic.DumpFromDirectory([in], save:=out)
+        Return 0
+    End Function
+End Module
